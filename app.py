2fba461d8f7e8bb9513cc706d563e1b4a8d9f5bd
vinitoni <88357365+vinitoni@users.noreply.github.com>
2024-11-01 21:22:47 -0300

    Update app.py

--- app.py
+++ app.py
@@ -12,13 +12,9 @@ app.secret_key = "toni123"
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME', 'vinitonii@gmail.com')
-app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD', 'qxgo eyfo jbms gcdi')
-app.config['MAIL_DEFAULT_SENDER'] = 'vinitonii@gmail.com'
-
-print(app.config['MAIL_USERNAME'])
-print(app.config['MAIL_PASSWORD'])  
-
+app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME', '')
+app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD', '')
+app.config['MAIL_DEFAULT_SENDER'] = ''
 
 mail = Mail(app)
 
